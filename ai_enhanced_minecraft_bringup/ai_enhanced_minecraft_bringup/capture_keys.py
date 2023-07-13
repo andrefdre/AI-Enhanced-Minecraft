517a0c58c688989ec752463d8f50bf6df1c9ee2c
Andre <andref@ua.pt>
2023-07-13 20:31:19 +0100

    added a script that listens to keys pressed and sends over a ros2 topic

--- ai_enhanced_minecraft_bringup/ai_enhanced_minecraft_bringup/capture_keys.py
+++ ai_enhanced_minecraft_bringup/ai_enhanced_minecraft_bringup/capture_keys.py
@@ -1,9 +1,35 @@
 #!/usr/bin/python3
 
+import time
+from pynput import keyboard
+from functools import partial
 
+# ROS2 imports
+import rclpy
+from rclpy.node import Node
+
+from std_msgs.msg import String
+
+class KeyCaptureNode(Node):
+    def __init__(self):
+        super().__init__('key_capture')
+        self.publisher = self.create_publisher(String, 'keys_pressed', 10)
+        self.on_press_partial = partial(self.on_press_callback, publisher=self.publisher)
+
+    def on_press_callback(self,key, publisher):
+        msg = String()
+        msg.data = str(key)
+        publisher.publish(msg)
 
 def main():
-    pass
+    rclpy.init()
+    node = KeyCaptureNode()
+
+    print("Press any key to start capturing keys...")
+    while True:
+        listener = keyboard.Listener(on_press=node.on_press_partial)
+        listener.start()
+        rclpy.spin(node)
 
 
 if __name__ == '__main__':
--- ai_enhanced_minecraft_bringup/setup.py
+++ ai_enhanced_minecraft_bringup/setup.py
@@ -22,6 +22,7 @@ setup(
         'console_scripts': [
             'publisher = ai_enhanced_minecraft_bringup.publisher:main',
             'listener = ai_enhanced_minecraft_bringup.listener:main',
+            'capture_keys = ai_enhanced_minecraft_bringup.capture_keys:main',
         ],
     },
 )
