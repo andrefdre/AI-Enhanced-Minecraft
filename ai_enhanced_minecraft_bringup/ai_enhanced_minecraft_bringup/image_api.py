20272458a4235d2b27b31f46cd43ab785e7103a5
Andre <andref@ua.pt>
2023-08-03 16:31:52 +0100

    implemented the script to retrieve the image from the rest api

---
+++ ai_enhanced_minecraft_bringup/ai_enhanced_minecraft_bringup/image_api.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+
+from functools import partial
+import requests
+import cv2
+import numpy as np
+
+# ROS2 imports
+import rclpy
+from rclpy.node import Node
+from sensor_msgs.msg import Image # Image is the message type
+from cv_bridge import CvBridge # Package to convert between ROS and OpenCV Images
+
+from std_msgs.msg import String
+
+class Image_API(Node):
+    def __init__(self):
+        super().__init__('minecraft_api')
+        self.publisher_ = self.create_publisher(Image, 'minecraft_frame', 10)
+        self.timer = self.create_timer(0, self.timer_callback)
+        self.image = None
+        self.window = cv2.namedWindow('image', cv2.WINDOW_NORMAL)
+        self.bridge = CvBridge()
+
+    def timer_callback(self):
+        try:
+            response = requests.get('http://localhost:8070/image')
+            self.image = response.content
+        except:
+            print("Error connecting to server")
+
+        image_array = cv2.imdecode(np.frombuffer(self.image, np.uint8), cv2.IMREAD_COLOR)
+        image_array = cv2.cvtColor(image_array, cv2.COLOR_BGR2RGB)
+
+        self.publisher_.publish(self.bridge.cv2_to_imgmsg(image_array))
+        cv2.imshow("image", image_array)
+    
+        cv2.waitKey(1)
+
+def main():
+    rclpy.init()
+    node = Image_API() 
+
+    rclpy.spin(node)
+
+    node.destroy_node()
+
+    rclpy.shutdown()
+    
+
+
+if __name__ == '__main__':
+    main()
